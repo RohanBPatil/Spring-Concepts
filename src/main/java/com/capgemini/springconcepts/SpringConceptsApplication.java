509b14289bf9abccc11492ad5d9d7bded1396f90
RohanBPatil <rohanpatil14218@gmail.com>
2020-12-08 22:36:26 +0530

    Added : Logger to log messages

--- src/main/java/com/capgemini/springconcepts/SpringConceptsApplication.java
+++ src/main/java/com/capgemini/springconcepts/SpringConceptsApplication.java
@@ -2,17 +2,21 @@ package com.capgemini.springconcepts;
 
 import com.capgemini.springconcepts.Component.DemoBean;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringConceptsApplication {
+	public static final Logger logger = LoggerFactory.getLogger(SpringConceptsApplication.class);
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to spring concept demo");
+		logger.debug("Welcome to spring concept demo");
 		ApplicationContext context = SpringApplication.run(SpringConceptsApplication.class, args);
-		System.out.println("Checking context: " + context.getBean(DemoBean.class));
+		DemoBean demoBean = context.getBean(DemoBean.class);
+		logger.info("DemoBean = " + demoBean.toString());
 	}
 
 }
